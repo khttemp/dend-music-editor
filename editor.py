3389f870c0619428fcd4c7fcbf2787f25ebd222d
khttemp <77556503a@gmail.com>
2022-05-11 20:55:22 +0900

    RS解釈、プレイヤー実装

--- editor.py
+++ editor.py
@@ -7,10 +7,17 @@ from tkinter import filedialog as fd
 from tkinter import ttk
 from tkinter import messagebox as mb
 from tkinter import simpledialog as sd
+import os
+from pygame import mixer
+import time
+
 from dendMusicDecrypt import RSMusicDecrypt as dendRs
 
+playback = None
 decryptFile = None
 frame = None
+after_id = None
+loopFlag = False
 
 LS = 0
 BS = 1
@@ -180,6 +187,126 @@ def openFile():
         deleteWidget()
         createWidget()
 
+def openBgmFile():
+    global bgmScale
+    global after_id
+    
+    file_path = fd.askopenfilename(filetypes=[("bgm", "*.ogg;*.wav")])
+    if file_path:
+        if after_id != None:
+            root.after_cancel(after_id)
+        file_name = os.path.splitext(os.path.basename(file_path))[0]
+        v_readBgm.set(file_name)
+        startEt["state"] = "readonly"
+        loopStartEt["state"] = "readonly"
+        loopEndEt["state"] = "readonly"
+        bgmScale["state"] = "disabled"
+        bgmPlayBtn["state"] = "disabled"
+        bgmPauseBtn["state"] = "disabled"
+        root.update()
+        
+        mixer.init()
+        mixer.music.load(file_path)
+        sound = mixer.Sound(file_path)
+        bgmScale["to"] = round(sound.get_length(), 2)
+        
+        startEt["state"] = "normal"
+        loopStartEt["state"] = "normal"
+        loopEndEt["state"] = "normal"
+        v_start.set(0.0)
+        v_loopStart.set(0.0)
+        v_loopEnd.set(-1.0)
+        val.set(0.0)
+        bgmScale["state"] = "normal"
+        bgmPlayBtn["state"] = "normal"
+        bgmPauseBtn["state"] = "disabled"
+
+
+def adjustPlayback():
+    global val
+    val.set(round(val.get(), 3))
+
+def playBgm():
+    global after_id
+    global loopFlag
+
+    try:
+        float(v_start.get())
+        float(v_loopStart.get())
+        float(v_loopEnd.get())
+    except:
+        errorMsg = "数字で入力してください。"
+        mb.showerror(title="エラー", message=errorMsg)
+        return
+
+    if v_start.get() < 0:
+        errorMsg = "startは、0以上で設定してください。"
+        mb.showerror(title="エラー", message=errorMsg)
+        return
+    if v_start.get() > bgmScale["to"]-0.5:
+        errorMsg = "startが曲の長さと近すぎ、あるいは超えています。"
+        mb.showerror(title="エラー", message=errorMsg)
+        return
+    if v_loopStart.get() < 0:
+        errorMsg = "loop startは、0以上で設定してください。"
+        mb.showerror(title="エラー", message=errorMsg)
+        return
+    if v_loopEnd.get() > 0 and v_loopStart.get() > v_loopEnd.get():
+        errorMsg = "loop startがloop endより大きいです。"
+        mb.showerror(title="エラー", message=errorMsg)
+        return
+
+    mixer.music.play(0, v_start.get())
+    val.set(round(v_start.get(), 3))
+    loopFlag = False
+    
+    bgmPlayBtn["state"] = "disabled"
+    bgmPauseBtn["state"] = "normal"
+    startEt["state"] = "readonly"
+    loopStartEt["state"] = "readonly"
+    loopEndEt["state"] = "readonly"
+    after_id = root.after(1, updateBgm)
+
+def pauseBgm():
+    global playback
+    global after_id
+    global loopFlag
+    
+    loopFlag = False
+    mixer.music.pause()
+    bgmPlayBtn["state"] = "normal"
+    bgmPauseBtn["state"] = "disabled"
+    startEt["state"] = "normal"
+    loopStartEt["state"] = "normal"
+    loopEndEt["state"] = "normal"
+    root.after_cancel(after_id)
+
+def updateBgm():
+    global after_id
+    global val
+    global loopFlag
+
+    fixTime = 0
+    if not loopFlag:
+        fixTime = v_start.get()
+    else:
+        fixTime = v_loopStart.get()
+        
+    val.set(round(fixTime + mixer.music.get_pos()/1000, 3))
+    if v_loopStart.get() <= v_loopEnd.get():
+        if fixTime + mixer.music.get_pos()/1000 >= v_loopEnd.get():
+            loopFlag = True
+            fixTime = v_loopStart.get()
+            mixer.music.play(0, v_loopStart.get())
+            val.set(round(fixTime + mixer.music.get_pos()/1000, 3))
+    
+    if round(fixTime + mixer.music.get_pos()/1000,3) >= bgmScale["to"]-0.01:
+        loopFlag = True
+        fixTime = v_loopStart.get()
+        mixer.music.play(0, fixTime)
+            
+    after_id = root.after(1, updateBgm)
+
 def createWidget():
     global decryptFile
     global bgmLf
@@ -252,6 +379,7 @@ root.geometry("1024x768")
 
 menubar = Menu(root)
 menubar.add_cascade(label='ファイルを開く', command= lambda: openFile())
+menubar.add_cascade(label='BGMファイルを開く', command= lambda: openBgmFile())
 root.config(menu=menubar)
 
 v_edit = StringVar()
@@ -282,7 +410,52 @@ rsRb.place(relx=0.22, rely=0.07)
 bgmLf = ttk.LabelFrame(root, text="BGMリスト")
 bgmLf.place(relx=0.03, rely=0.12, relwidth=0.94, relheight=0.5)
 
-#perfLf = ttk.LabelFrame(root, text="性能")
-#perfLf.place(relx=0.45, rely=0.12, relwidth=0.52, relheight=0.8)
+playerLf = ttk.LabelFrame(root, text="プレイヤー")
+playerLf.place(relx=0.03, rely=0.63, relwidth=0.94, relheight=0.35)
+
+startLb = ttk.Label(playerLf, text="start", font=("", 20), relief="groove", anchor=CENTER)
+startLb.place(relx=0.03, rely=0.1, relwidth=0.1, relheight=0.205)
+v_start = DoubleVar()
+v_start.set(0.0)
+startEt = ttk.Entry(playerLf, textvariable=v_start, font=("", 20), justify=CENTER)
+startEt.place(relx=0.13, rely=0.1, relwidth=0.13, relheight=0.2)
+
+loopStartLb = ttk.Label(playerLf, text="loop start", font=("", 20), relief="groove", anchor=CENTER)
+loopStartLb.place(relx=0.31, rely=0.1, relwidth=0.18, relheight=0.205)
+v_loopStart = DoubleVar()
+v_loopStart.set(0.0)
+loopStartEt = ttk.Entry(playerLf, textvariable=v_loopStart, font=("", 20), justify=CENTER)
+loopStartEt.place(relx=0.49, rely=0.1, relwidth=0.13, relheight=0.2)
+
+loopEndLb = ttk.Label(playerLf, text="loop end", font=("", 20), relief="groove", anchor=CENTER)
+loopEndLb.place(relx=0.66, rely=0.1, relwidth=0.18, relheight=0.205)
+v_loopEnd = DoubleVar()
+v_loopEnd.set(-1.0)
+loopEndEt = ttk.Entry(playerLf, textvariable=v_loopEnd, font=("", 20), justify=CENTER)
+loopEndEt.place(relx=0.84, rely=0.1, relwidth=0.13, relheight=0.2)
+
+startEt["state"] = "readonly"
+loopStartEt["state"] = "readonly"
+loopEndEt["state"] = "readonly"
+
+val = DoubleVar()
+bgmScale = ttk.Scale(playerLf, variable=val, from_=0, to=100, command=lambda e: adjustPlayback())
+bgmScale.place(relx=0.03, rely=0.4, relwidth=0.85)
+
+bgmValueLb = ttk.Label(playerLf, textvariable=val, font=("", 20))
+bgmValueLb.place(relx=0.90, rely=0.4)
+
+bgmScale["state"] = "disabled"
+
+v_readBgm = StringVar()
+bgmPlayNameLb = ttk.Label(playerLf, textvariable=v_readBgm, font=("", 14), relief="groove", anchor=CENTER)
+bgmPlayNameLb.place(relx=0.03, rely=0.6, relwidth=0.28, relheight=0.2)
+
+bgmPlayBtn = ttk.Button(playerLf, text="Play", command=playBgm)
+bgmPlayBtn.place(relx=0.40, rely=0.6, relwidth=0.2, relheight=0.2)
+bgmPlayBtn["state"] = "disabled"
+bgmPauseBtn = ttk.Button(playerLf, text="Pause", command=pauseBgm)
+bgmPauseBtn.place(relx=0.68, rely=0.6, relwidth=0.2, relheight=0.2)
+bgmPauseBtn["state"] = "disabled"
 
 root.mainloop()
